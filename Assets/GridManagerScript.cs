274d0d44107915604b8fe56ac61d7c84e6b9b385
NocaWu <noca@nyu.edu>
2018-12-06 18:45:27 -0500

    Initial Commit

---
+++ Assets/GridManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManagerScript : MonoBehaviour {
+
+    public static GridManagerScript instance;
+
+    public GameObject tilePrefab;
+    private GameObject[,] tile= new GameObject[5,5]; // the whole grid(5*5) tiles as an array
+
+    public Sprite[] tileSprite;
+
+    public bool isMoving;
+
+	// Use this for initialization
+	void Start () {
+        instance = GetComponent<GridManagerScript>();
+
+        // spawn a 5 * 5 grid
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                tile[x,y] = Instantiate(tilePrefab, new Vector3(x - 2, y - 2, 0), Quaternion.identity);
+                tile[x,y].transform.parent = gameObject.transform;
+                tile[x,y].GetComponent<SpriteRenderer>().sprite = tileSprite[Random.Range(0,tileSprite.Length)];
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    //public static TileScript selectedTile;
+    private SpriteRenderer rend;
+    private bool isSelected;
+    private Color selectedColor = new Vector4(0.5f,0.5f,0.5f,0.5f);
+
+	void Start()
+	{
+        rend = GetComponent<SpriteRenderer>();
+	}
+
+    void OnMouseDown()
+	{
+        if(rend.sprite && !GridManagerScript.instance.isMoving)
+        {
+            if (isSelected == true)
+            {
+                isSelected = false;
+                rend.color = Color.white;
+            }
+            else
+            {
+                isSelected = true;
+                rend.color = selectedColor;
+            } 
+        }
+	}
+}
