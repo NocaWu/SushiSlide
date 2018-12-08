43f991f71d90565c71d62495c951f7a92ca0e1fb
NocaWu <noca@nyu.edu>
2018-12-07 20:59:28 -0500

    object swap working

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+    public static GameManagerScript instance;
+    GameObject selectOne = null;
+    GameObject selectTwo = null;
+	// Use this for initialization
+	void Start () {
+        instance = GetComponent<GameManagerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/TileScript.cs
+++ Assets/TileScript.cs
@@ -18,43 +18,45 @@ public class TileScript : MonoBehaviour {
 	{
         isSelected = true;
         rend.color = selectedColor;
+        prevSelected = gameObject.GetComponent<TileScript>();
 	}
 
     private void Deselect()
     {
         isSelected = false;
         rend.color = Color.white;
-        prevSelected.rend.color = Color.white;
         prevSelected = null;
     }
 
-	void OnMouseDown()
-	{
-        if(/*rend.sprite && */!GridManagerScript.instance.isMoving)
-        {
-            if (isSelected == true)
-            {
-                Deselect();
-            }
-            else if (prevSelected == null)
-            {
-                prevSelected = gameObject.GetComponent<TileScript>();
-                Select();
-            } 
-            else 
-            {
-                //Swap(prevSelected.rend);
-                SwapObject(prevSelected.gameObject);
-                Deselect();
-            }
-        }
-	}
+    void OnMouseDown()
+    {
+           if(rend.sprite && !GridManagerScript.instance.isMoving)
+           {
+               if (isSelected == true)
+               {
+                   Deselect();
+               }
+               else if (prevSelected == null)
+               {
+                   Select();
+               } 
+               else
+               {
+                   //Swap(prevSelected.rend);
+                   SwapObject(prevSelected.gameObject);
+                   prevSelected.Deselect();
+               }
+           }
+    }
+
 
     void Swap(SpriteRenderer prev)
     {
-        float xDis = gameObject.transform.position.x - prev.gameObject.transform.position.x;
-        float yDis = gameObject.transform.position.y - prev.gameObject.transform.position.y;
+        float xDis = transform.position.x - prev.gameObject.transform.position.x;
+        float yDis = transform.position.y - prev.gameObject.transform.position.y;
+
         SpriteRenderer self = gameObject.GetComponent<SpriteRenderer>();
+
         Sprite temp;
 
         if (Mathf.Abs(xDis) < 1.1f && Mathf.Abs(yDis) < 1.1f && prev.sprite != self.sprite)
@@ -67,19 +69,36 @@ public class TileScript : MonoBehaviour {
 
     void SwapObject(GameObject prev)
     {
-       //Sprite prevSprite = prev.GetComponent<SpriteRenderer>().sprite;
-        //Sprite selfSprite = GetComponent<SpriteRenderer>().sprite;
-        Vector3 selfPos = transform.position;
-        Vector3 prevPos = prev.transform.position;
+        Sprite prevSprite = prev.GetComponent<SpriteRenderer>().sprite;
+        Sprite selfSprite = GetComponent<SpriteRenderer>().sprite;
+       
         Vector3 tempPos;
-        //float xDis = selfPos.x - prevPos.x;
-        //float yDis = selfPos.y - prevPos.y;
 
-        //if (Mathf.Abs(xDis) < 1.1f && Mathf.Abs(yDis) < 1.1f && prevSprite != selfSprite)
-       // {
-            tempPos = prevPos;
-            prevPos = selfPos;
-            selfPos = tempPos;
-        //}
+        float xDis = transform.position.x - prev.transform.position.x;
+        float yDis = transform.position.y - prev.transform.position.y;
+
+        if (Mathf.Abs(xDis) < 1.1f && Mathf.Abs(yDis) < 1.1f && prevSprite != selfSprite)
+        {
+            //tempPos = prev.transform.position;
+            //prev.transform.position = transform.position;
+            //transform.position = tempPos;
+            StartCoroutine(SmoothSwap(prev));
+        }
+    }
+
+    IEnumerator SmoothSwap(GameObject prev)
+    {
+        GridManagerScript.instance.isMoving = true;
+        Vector3 tempPrev = prev.transform.position;
+        Vector3 tempSelf = transform.position;
+
+        for (float f = 0f; f <= 1; f += 0.1f)
+        {
+            prev.transform.position = Vector3.Lerp(prev.transform.position, tempSelf,f*f);
+            transform.position = Vector3.Lerp(transform.position,tempPrev,f*f);
+            yield return null;
+        }
+
+        GridManagerScript.instance.isMoving = false;
     }
 }
