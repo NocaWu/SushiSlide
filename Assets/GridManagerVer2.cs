40856567d2663d73932f9fb362098230d6cfbd4d
NocaWu <noca@nyu.edu>
2018-12-10 11:35:48 -0500

    rewrote tile and grid behavior
    tiles find matches, grid clear matched tiles, grid shift and refill itself untill no match on the board

--- Assets/GridManagerVer2.cs
+++ Assets/GridManagerVer2.cs
@@ -4,30 +4,72 @@ using UnityEngine;
 
 public class GridManagerVer2 : MonoBehaviour {
 
-    public static GridManagerScript instance;
+    public static GridManagerVer2 instance;
 
     public GameObject tilePrefab;
     public GameObject[,] tile = new GameObject[5, 5]; // the whole grid(5*5) tiles as an array
 
     public Sprite[] tileSprite;
 
-    public bool isMoving;
+    public bool isShifting;
 
     // Use this for initialization
     void Awake()
     {
-        instance = GetComponent<GridManagerScript>();
+        instance = GetComponent<GridManagerVer2>();
+        FillGrid();
+    }
 
-        // spawn a 5 * 5 grid
+	void Update()
+	{
+        ClearMatch();
+        ShiftGrid();
+        FillGrid();
+	}
+
+    void FillGrid()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                if (!tile[x, y])
+                {
+                    tile[x, y] = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    tile[x, y].transform.parent = gameObject.transform;
+                    tile[x, y].GetComponent<SpriteRenderer>().sprite = tileSprite[Random.Range(0, tileSprite.Length)];
+                }
+            }
+        } 
+    }
+
+    void ClearMatch()
+    {
         for (int x = 0; x < 5; x++)
         {
             for (int y = 0; y < 5; y++)
             {
-                tile[x, y] = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
-                tile[x, y].transform.parent = gameObject.transform;
-                tile[x, y].GetComponent<SpriteRenderer>().sprite = tileSprite[Random.Range(0, tileSprite.Length)];
+                if (tile[x, y] && tile[x, y].GetComponent<TileBehaviorVer2>().inMatch == true)
+                {
+                    Destroy(tile[x, y]);
+                }
             }
         }
     }
 
+    void ShiftGrid()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if (!tile[x, y] && tile[x, y + 1])
+                {
+                    tile[x, y + 1].transform.position -= new Vector3(0, 1, 0);
+                    tile[x, y] = tile[x, y + 1];
+                    tile[x, y + 1] = null;
+                }
+            }
+        }
+    }
 }
--- Assets/TileBehaviorVer2.cs
+++ Assets/TileBehaviorVer2.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileBehaviorVer2 : MonoBehaviour {
+public class TileBehaviorVer2 : MonoBehaviour
+{
+    public bool inMatch = false;
+    private Color selectedColor = new Vector4(1, 1, 1, 0.5f);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindMatchTiles();
+    }
+
+    void FindMatchTiles()
+    {
+        List<GameObject> matchTiles = new List<GameObject>();
+        List<GameObject> neighbourTiles = new List<GameObject>();
+
+        int xPos = Mathf.RoundToInt(transform.position.x);
+        int yPos = Mathf.RoundToInt(transform.position.y);
+
+        if (yPos <= 3)
+        {
+            GameObject tileAbove = GridManagerVer2.instance.tile[xPos, yPos + 1];
+            neighbourTiles.Add(tileAbove);
+        }
+        if (yPos >= 1)
+        {
+            GameObject tileBelow = GridManagerVer2.instance.tile[xPos, yPos - 1];
+            neighbourTiles.Add(tileBelow);
+        }
+        if (xPos <= 3)
+        {
+            GameObject tileToright = GridManagerVer2.instance.tile[xPos + 1, yPos];
+            neighbourTiles.Add(tileToright);
+        }
+        if (xPos >= 1)
+        {
+            GameObject tileToleft = GridManagerVer2.instance.tile[xPos - 1, yPos];
+            neighbourTiles.Add(tileToleft);
+        }
+
+        for (int i = 0; i < neighbourTiles.Count; i++)
+        {
+            if (neighbourTiles[i])
+            {
+                if (neighbourTiles[i].GetComponent<SpriteRenderer>().sprite == gameObject.GetComponent<SpriteRenderer>().sprite)
+                {
+                    matchTiles.Add(neighbourTiles[i]);
+                }
+            }
+        }
+
+        if (matchTiles.Count >= 2)
+        {
+            inMatch = true;
+            for (int i = 0; i < matchTiles.Count; i++)
+            {
+                matchTiles[i].GetComponent<TileBehaviorVer2>().inMatch = true;
+            }
+        }
+    }
 }
